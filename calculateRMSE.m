b3bc3594c8ac8d629e813b1feb62d23c53d1133f
TalaatHarb <mohamed.talaatharb@eng.asu.edu.eg>
2019-07-13 21:12:00 +0200

    Fixed RMSE

--- calculateRMSE.m
+++ calculateRMSE.m
@@ -1,9 +1,5 @@
 function rmse = calculateRMSE(otherMatrix, matrix)
-  [m, n] = size(matrix);
   diff = matrix(:) - otherMatrix(:);
   diffSquared = diff .* diff;
-  rmse = sqrt(sum(diffSquared(:)) / (m*n));
-  if(size(matrix, 3) == 3)
-    rmse = rmse / sqrt(3);
-  end
+  rmse = sqrt(sum(diffSquared(:))) / sqrt(prod(size(matrix)));
 end
